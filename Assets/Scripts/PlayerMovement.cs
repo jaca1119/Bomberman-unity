61f211faed4801d83314b29a5126023d11bdbae1
jaca1119 <laki1663@gmail.com>
2020-06-28 18:25:22 +0200

    Animations, basic snap bomb

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
 
+    public GameObject bomb;
+
     private Rigidbody2D rb2d;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+    private CapsuleCollider2D collider2D;
+
 
     private float deltaMovement = 0.01f;
     private bool movingRight = true;
@@ -19,6 +23,12 @@ public class PlayerMovement : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        collider2D = GetComponent<CapsuleCollider2D>();
+    }
+
+    private void Update()
+    {
+        HandleBombPlaceing();
     }
 
     void FixedUpdate()
@@ -53,6 +63,34 @@ public class PlayerMovement : MonoBehaviour
         spriteRenderer.flipX = movingRight;
 
         movingRight = !movingRight;
+    }
+
+    private void HandleBombPlaceing()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (bomb)
+            {
+                Debug.Log(transform.position);
+                Vector3 snap = new Vector3(Mathf.RoundToInt(transform.position.x) - 0.5f,
+                                              Mathf.RoundToInt(transform.position.y) - 0.5f,
+                                              Mathf.RoundToInt(transform.position.z));
+                Debug.Log(snap);
+
+                float snapValue = 0.5f;
+                float snapInverse = 1 / snapValue;
+                float x, y, z;
+
+
+                x = Mathf.Round(collider2D.bounds.center.x * snapInverse) / snapInverse;
+                y = Mathf.Round(collider2D.bounds.center.y * snapInverse) / snapInverse;
+                z = 0;
+
+                Debug.Log(new Vector3(x, y, z));
+                Instantiate(bomb, new Vector3(x, y, z), bomb.transform.rotation);
+            }
+        }
+
     }
 
 }
