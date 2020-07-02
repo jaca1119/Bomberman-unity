3ac727c5e5fa9221c573348a27c3a98b67cb306b
jaca1119 <laki1663@gmail.com>
2020-07-02 13:11:32 +0200

    Added some UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,4 +9,9 @@ public class UIManager : MonoBehaviour
     {
         SceneManager.LoadScene("SampleScene");
     }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
